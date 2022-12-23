858aba610226bb05090e477d71e26851203d2e1a
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-23 15:46:27 +0100

    Add array di immagini e struttura ricreata in js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@
         1. Creare i bottoni per lo scorrimento delle immagini
         2. Creare un costrutto per ciascuno dei bottoni in modo che ad ogni click l'immagine mostrata cambi
 */
+
+// MILESTONE 2
+// ricreazione struttura
+const body = document.querySelector('body');
+const main = document.createElement('main');
+body.append(main);
+let carouselDiv = document.createElement('div');
+carouselDiv.className = 'carousel';
+main.append(carouselDiv);
+console.log('Debug ok');
+
+let subCarousel = document.createElement('div');
+carouselDiv.append(subCarousel);
+console.log('Debug ok');
+
+let slidesDiv = document.createElement('div');
+slidesDiv.className = 'slides';
+let buttonsDiv = document.createElement('div');
+buttonsDiv.className = 'buttons';
+subCarousel.append(slidesDiv, buttonsDiv);
+console.log('Debug ok');
+
+const pics = ["consegna/img/01.webp",
+              "consegna/img/02.webp",
+              "consegna/img/03.webp",
+              "consegna/img/04.webp",
+              "consegna/img/05.webp"];
+
+for (let i = 0; i < pics.lenght; i++); {
+    
+}
