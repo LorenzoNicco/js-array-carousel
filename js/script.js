d4b7d49bd91b23c46af13eb17e1236e0744fc5d6
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-23 14:46:33 +0100

    Inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+    STEPS
+    MILESTONE 1
+        1. Inizio
+        2. Creare la struttura base in html
+        3. Inserire un'immagine grande al suo interno
+        4. Stabilire la stilizzazione tramite css
+    MILESTONE 2
+        1. Rimuovere il markup statico
+        2. Ricreare la struttura usando js
+        3. Creare un array di immagini
+        4. Inserire l'array in un ciclo per concatenare le immagini
+        5. Assegnare una classe con display block solo alla prima immagine, le altre saranno display none
+    MILESTONE 3
+        1. Creare i bottoni per lo scorrimento delle immagini
+        2. Creare un costrutto per ciascuno dei bottoni in modo che ad ogni click l'immagine mostrata cambi
+*/
